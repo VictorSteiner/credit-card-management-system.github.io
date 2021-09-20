65cde038cc6d5025594716af26be2b946ddbbb1d
einar1996 <einarjorgen.hansson@gmail.com>
2021-09-20 18:19:06 +0200

    credit card service

--- src/app/services/credit-card-service.service.ts
+++ src/app/services/credit-card-service.service.ts
@@ -21,7 +21,24 @@ export class CreditCardServiceService {
 
   createClient(creditCard: CreditCard) {
     this.http.post<CreditCardResult>(`${this.rootUrl}/credit_cards`, creditCard).pipe(
-
+      catchError((e : HttpErrorResponse) => {
+        return of({messeage: e.message, status: e.status})
+      })
     )
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
