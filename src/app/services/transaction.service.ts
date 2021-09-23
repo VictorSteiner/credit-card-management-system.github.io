2a74608f700a03a2f617e43b71cf889df263927e
einar1996 <einarjorgen.hansson@gmail.com>
2021-09-23 23:52:53 +0200

    Fix to use the new version of transaction

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators'
-import { Transaction } from '../models/transaction/transaction.model';
+import { Transaction } from '../models/transaction.type';
 import { HttpHandlingService } from './http-handling.service';
 
 @Injectable({
