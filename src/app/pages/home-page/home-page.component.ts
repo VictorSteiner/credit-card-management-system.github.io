13d424783a5f886830a695f668f0e0b5128e025c
einar1996 <einarjorgen.hansson@gmail.com>
2021-09-23 23:53:30 +0200

    added pagination

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { CreditCard } from 'src/app/models/credit-card.type';
 import { CreditCardService } from 'src/app/services/credit-card-service.service';
@@ -12,14 +15,21 @@ export class HomePageComponent implements OnInit {
 
   creditCards$ : Observable<CreditCard[]> | null = null;
   displayedColumns: string[] = ['position','card_number', 'cardholder_name', 'issuer']
-  constructor(private creditCardService : CreditCardService) { }
+  dataSource = new MatTableDataSource()
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor(private creditCardService : CreditCardService, private router: Router) { }
 
   ngOnInit(): void {
-    this.creditCards$ = this.creditCardService.getCreditCards();
+    this.creditCardService.credit_cards$.subscribe(creditCards => {this.dataSource.data = creditCards});
+  }
+  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
-  rowClick(row: number): void {
-    console.log(row);
+  rowClick(id: number): void {
+    this.router.navigate(['/credit-card', id]);
   }
 
 }
