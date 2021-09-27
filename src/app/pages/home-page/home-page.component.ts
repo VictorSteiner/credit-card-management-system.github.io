ef8d49d4a77d8ddeceb0edc0c284c9ce75a02799
Einar Hansson <einarjorgen.hansson@gmail.com>
2021-09-27 15:40:20 +0200

    Lint

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,1 @@
-import { Component, ViewChild } from '@angular/core';
-
-@Component({
-	selector: 'app-home-page',
-	templateUrl: './home-page.component.html',
-	styleUrls: ['./home-page.component.css'],
-})
-export class HomePageComponent {
-	constructor() {}
-}
-
+import { Component, ViewChild } from '@angular/core';@Component({	selector: 'app-home-page',	templateUrl: './home-page.component.html',	styleUrls: ['./home-page.component.css'],})export class HomePageComponent {	constructor() {}}
