b2e2e582b4b3e596b0f692efd3188d41ed8c86d5
einar1996 <einarjorgen.hansson@gmail.com>
2021-09-27 14:21:47 +0200

    removed add credit card page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
 import { LuxonModule } from 'luxon-angular';
-import { AddCreditCardPageComponent } from './pages/add-credit-card-page/add-credit-card-page.component';
 import { CreditCardModule } from './credit-card-module/credit-card.module';
 import { SharedModule } from './shared/shared.module';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -16,7 +15,6 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [
     AppComponent,
-    AddCreditCardPageComponent,
     TransactionPageComponent,
     HomePageComponent,
   ],
