e4549227f4d83c4e1a66ade9b0946c9bd7ee9940
einar1996 <39279985+einar1996@users.noreply.github.com>
2021-10-04 13:54:20 +0200

    Feature/deletecard (#24)
    * created module
    
    * Created
    
    * Added deletefunction
    
    * Added delete card
    
    * Delete card
    
    * format

---
+++ src/app/transaction-module/transactionadd/transactionadd.component.ts
@@ -0,0 +1,1 @@
+import { Component, OnInit } from '@angular/core';@Component({	selector: 'app-transactionadd',	templateUrl: './transactionadd.component.html',	styleUrls: ['./transactionadd.component.css'],})export class TransactionaddComponent implements OnInit {	showFiller: boolean = false;	constructor() {}	ngOnInit(): void {		console.log('test');	}}
