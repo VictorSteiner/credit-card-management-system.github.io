6e0c8e8115eb3af54fe78f4bd9e2877e6e142dae
VictorSteiner <victorsteiner91@gmail.com>
2021-10-14 19:11:08 +0200

    added form

--- src/app/transaction-module/transactionadd/transactionadd.component.ts
+++ src/app/transaction-module/transactionadd/transactionadd.component.ts
@@ -1,1 +1,42 @@
-import { Component, OnInit } from '@angular/core';@Component({	selector: 'app-transactionadd',	templateUrl: './transactionadd.component.html',	styleUrls: ['./transactionadd.component.css'],})export class TransactionaddComponent implements OnInit {	showFiller: boolean = false;	constructor() {}	ngOnInit(): void {		console.log('test');	}}
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { CreditCard } from 'src/app/models/credit-card.type';
+import { CURRENCIES } from 'src/app/models/transaction.type';
+import { CreditCardService } from 'src/app/services/credit-card-service.service';
+import { TransactionService } from 'src/app/services/transaction.service';
+
+@Component({
+	selector: 'app-transactionadd',
+	templateUrl: './transactionadd.component.html',
+	styleUrls: ['./transactionadd.component.css'],
+})
+export class TransactionaddComponent implements OnInit {
+	formGroup = new FormGroup({
+		creditCardNumber: new FormControl(null),
+		amount: new FormControl(null),
+		comment: new FormControl(null),
+		date: new FormControl(null),
+		currency: new FormControl(null),
+	});
+
+	currencies = CURRENCIES;
+
+	$creditCards: Observable<CreditCard[]>;
+
+	constructor(
+		private service: TransactionService,
+		private creditCardService: CreditCardService
+	) {}
+
+	onSubmit = (formGroup: FormGroup): void => {
+		this.service.postTransaction({
+			date: Date.parse(formGroup.value.date),
+			...formGroup.value,
+		});
+	};
+
+	ngOnInit(): void {
+		this.$creditCards = this.creditCardService.creditCards$;
+	}
+}
